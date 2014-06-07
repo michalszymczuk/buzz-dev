04ac0c99ff4e84ba0e9e46b3fa5d67b84885c8be
michalszymczuk <michal.szymczuk@gmail.com>
2014-06-07 23:26:36 +0200

    z

--- js/index.js
+++ js/index.js
@@ -1,9 +1,45 @@
-﻿function onLoad()
-{
+﻿function onLoad(){
     document.addEventListener('deviceready', onDeviceReady, false);
 }
-function onDeviceReady()
-{
-    // do nothing
+function onDeviceReady(){
     alert('Device is now ready');
+}
+
+var app = {
+  // constructor 
+  initialize: function() {
+    var pushNotification = window.plugins.pushNotification;
+    pushNotification.register(app.successHandler, app.errorHandler,{"senderID":"628189894548","ecb":"app.onNotificationGCM"});
+  },
+   successHandler: function(result) {
+    alert('Callback Success! Result = '+result)
+  },
+  errorHandler:function(error) {
+    alert('pushNorification.register error: ' +error);
+  },
+  onNotificationGCM: function(e) {
+    switch( e.event )
+    {
+      case 'registered':
+        if ( e.regid.length > 0 )
+        {
+            console.log("Regid " + e.regid);
+            alert('registration id = '+e.regid);
+        }
+        break;
+
+      case 'message':
+        // this is the actual push notification. its format depends on the data model from the push server
+        alert('message = '+e.message+' msgcnt = '+e.msgcnt);
+        break;
+
+      case 'error':
+        alert('GCM error = '+e.msg);
+        break;
+
+      default:
+        alert('An unknown GCM event has occurred');
+        break;
+    }
+  }
 }
